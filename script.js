a07e27ee33d93294f3c4414774ece4c72fac92fc
adarsh_dayanand <ad.29.1999@gmail.com>
2022-05-28 14:33:16 +0530

    Added auth

--- script.js
+++ script.js
@@ -1,57 +1,74 @@
 (function () {
-    
-    // Your web app's Firebase configuration
-    const firebaseConfig = {
-        apiKey: "AIzaSyBdvg80FoKCxrRJrNnHJZCmVqeHg9BPQCU",
-        authDomain: "sa-oth-ada18.firebaseapp.com",
-        projectId: "sa-oth-ada18",
-        storageBucket: "sa-oth-ada18.appspot.com",
-        messagingSenderId: "305380503193",
-        appId: "1:305380503193:web:03a3b89e993b620a1f9208",
-        measurementId: "G-7C0N7S4151",
-      };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+  let person = prompt("Please enter the login id");
+  let divContainer = document.getElementById("container").style.display;
 
-    //Get Elements
-    const txtEmail = document.getElementById("txtEmail");
-    const txtPassword = document.getElementById("txtPassword");
-    const btnLogin = document.getElementById("btnLogin");
-    const btnSignup = document.getElementById("btnSignup");
+  const txtEmail = document.getElementById("txtEmail");
+  const txtPassword = document.getElementById("txtPassword");
+  const btnLogin = document.getElementById("btnLogin");
+  const btnSignup = document.getElementById("btnSignup");
+  const emailLabel = document.getElementById("email-label");
+  const passwordLabel = document.getElementById("passowrd-label");
 
-    //Add Login Event
-    // btnLogin.addEventListener('click', e => {
-    //     const email = txtEmail.value;
-    //     const password = txtPassword.value;
+  if (person === "sa-oth") {
+    divContainer = "block";
+  } else {
+      alert('Invaid Auth')
+    divContainer = "none";
+    txtEmail.style.display = "none";
+    txtPassword.style.display = "none";
+    btnLogin.style.display = "none";
+    btnSignup.style.display = "none";
+    emailLabel.style.display = "none";
+    passwordLabel.style.display = "none";
+  }
 
-    //     const auth = firebase.auth();
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBdvg80FoKCxrRJrNnHJZCmVqeHg9BPQCU",
+    authDomain: "sa-oth-ada18.firebaseapp.com",
+    projectId: "sa-oth-ada18",
+    storageBucket: "sa-oth-ada18.appspot.com",
+    messagingSenderId: "305380503193",
+    appId: "1:305380503193:web:03a3b89e993b620a1f9208",
+    measurementId: "G-7C0N7S4151",
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-    //     //sign in with firebase auth
-    //     auth.signInWithEmailAndPassword(email, password).then(user =>{
-    //         alert("Login Successful :)");
-    //     }).catch(err => {
-    //         alert(err.message);
-    //     });
-        
-    // });
+  //Get Elements
 
-    //Add Signup Event
-    btnSignup.addEventListener('click', e => {
+  //Add Login Event
+  // btnLogin.addEventListener('click', e => {
+  //     const email = txtEmail.value;
+  //     const password = txtPassword.value;
 
-        //get email and password
-        const email = txtEmail.value;
-        const password = txtPassword.value;
+  //     const auth = firebase.auth();
 
-        const auth = firebase.auth();
+  //     //sign in with firebase auth
+  //     auth.signInWithEmailAndPassword(email, password).then(user =>{
+  //         alert("Login Successful :)");
+  //     }).catch(err => {
+  //         alert(err.message);
+  //     });
 
-        //sign in with firebase auth
-        const promise = auth.createUserWithEmailAndPassword(email, password).then(user => {
-            alert("Signup Successful :)")
-        }).catch(err => {
-            alert(err.message);
-        });
+  // });
 
-    });
+  //Add Signup Event
+  btnSignup.addEventListener("click", (e) => {
+    //get email and password
+    const email = txtEmail.value;
+    const password = txtPassword.value;
 
+    const auth = firebase.auth();
 
-}());
+    //sign in with firebase auth
+    const promise = auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((user) => {
+        alert("Signup Successful :)");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  });
+})();
