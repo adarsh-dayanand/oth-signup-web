528536b0d157258cb96b0dfc8f97ff9a08fca5c5
adarsh_dayanand <ad.29.1999@gmail.com>
2022-05-29 20:03:51 +0530

    Update UI

--- script.js
+++ script.js
@@ -4,23 +4,31 @@
 
   const txtEmail = document.getElementById("txtEmail");
   const txtPassword = document.getElementById("txtPassword");
-  // const btnLogin = document.getElementById("btnLogin");
+  const confirmPassword = document.getElementById("confirmPassword");
   const btnSignup = document.getElementById("btnSignup");
   const emailLabel = document.getElementById("email-label");
   const passwordLabel = document.getElementById("passowrd-label");
+  const messageSuccess = document.getElementById("message-success");
+  const messageInfo = document.getElementById("message-info");
+  const messageError = document.getElementById("message-error");
 
   if (person === "sa-oth") {
-    alert('Welcome');
+    messageSuccess.style.display = "none";
+    messageInfo.style.display = "none";
+    messageError.style.display = "none";
     divContainer = "block";
   } else {
     alert("Invaid Auth");
     divContainer = "none";
     txtEmail.style.display = "none";
     txtPassword.style.display = "none";
-    // btnLogin.style.display = "none";
+    confirmPassword.style.display = "none";
     btnSignup.style.display = "none";
     emailLabel.style.display = "none";
     passwordLabel.style.display = "none";
+    messageSuccess.style.display = "none";
+    messageInfo.style.display = "none";
+    messageError.style.display = "none";
   }
 
   // Your web app's Firebase configuration
@@ -67,44 +75,58 @@
     //get email and password
     const email = txtEmail.value;
     const password = txtPassword.value;
+    const confirmPasswordval = confirmPassword.value;
 
-    const auth = firebase.auth();
-    const db = firebase.firestore();
+    if (password === confirmPasswordval) {
+      const auth = firebase.auth();
+      const db = firebase.firestore();
 
-    //sign in with firebase auth
-    auth
-      .createUserWithEmailAndPassword(email, password)
-      .then((user) => {
-        alert("Signup Successful :) ");
+      //sign in with firebase auth
+      auth
+        .createUserWithEmailAndPassword(email, password)
+        .then((user) => {
+          let numberOfUsers = 0;
 
-        let numberOfUsers = 0;
+          db.collection("users")
+            .get()
+            .then((snap) => {
+              numberOfUsers = snap.size;
 
-        db.collection("users")
-          .get()
-          .then((snap) => {
-            numberOfUsers = snap.size;
+              db.collection("users")
+                .doc(user.user.email)
+                .set({
+                  email: user.user.email,
+                  id: numberOfUsers + 1,
+                  timer: "",
+                })
+                .then((docRef) => {
+                  console.log("Document written with ID: ", docRef.id);
+                })
+                .catch((error) => {
+                  console.error("Error adding document: ", error);
+                });
+            });
 
-            db.collection("users")
-              .doc(user.user.email)
-              .set({
-                email: user.user.email,
-                id: numberOfUsers + 1,
-                timer: "",
-              })
-              .then((docRef) => {
-                console.log("Document written with ID: ", docRef.id);
-              })
-              .catch((error) => {
-                console.error("Error adding document: ", error);
-              });
-          });
+          messageSuccess.style.display = "block";
+          messageInfo.style.display = "none";
+          messageError.style.display = "none";
 
-        document.getElementById("registered-email").innerHTML =
-          user.user.email + " is successfully registered.";
-      })
-      .catch((err) => {
-        console.log(err);
-        document.getElementById("registered-email").innerHTML = err.message;
-      });
+          document.getElementById("message-success").innerHTML =
+            user.user.email + " is successfully registered.";
+        })
+        .catch((err) => {
+          console.log(err);
+          messageSuccess.style.display = "none";
+          messageInfo.style.display = "none";
+          messageError.style.display = "block";
+          document.getElementById("message-error").innerHTML = err.message;
+        });
+    } else {
+      messageSuccess.style.display = "block";
+      messageInfo.style.display = "none";
+      messageError.style.display = "block";
+      document.getElementById("message-error").innerHTML =
+        "Passwords donot match";
+    }
   });
 })();
